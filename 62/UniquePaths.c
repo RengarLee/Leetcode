ae86f538cb506afe98eb42cfb2960878358d22b8
Rengar Lee <RengarLee@163.com>
2020-12-15 18:02:15 +0800

    UniquePaths

---
+++ 62/UniquePaths.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+// int UniquePath(int m, int n)
+// {
+//     int path[m][n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         path[i][0] = 1;
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         path[0][i] = 1;
+//     }
+//     for (int i = 1; i < m; i++)
+//     {
+//         for (int j = 1; j < n; j++)
+//         {
+//             path[i][j] = path[i-1][j]+path[i][j-1];
+//         }
+//     }
+//     return path[m-1][n-1];    
+// }
+
+int UniquePath(int m, int n)
+{
+    int path[n];
+    for (int i = 0; i < n; i++)
+    {
+        path[i] = 1;
+    }
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            path[j] += path[j-1];
+        }
+    }
+    return path[n-1];    
+}
+
+int main()
+{
+    printf("%d\n", UniquePath(2,3));
+    return 0;
+}
