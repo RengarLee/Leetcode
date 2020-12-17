5cf2f43d99f34cf2bf08261fab5df6c641d3bb1b
Rengar Lee <RengarLee@163.com>
2020-12-17 21:59:50 +0800

    fix

--- 64/MinimumPathSum.c
+++ 64/MinimumPathSum.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+// int minPathSum(int **grid, int gridSize, int *gridColSize)
+// {
+//     int n = gridSize;
+//     int m = *gridColSize;
+//     int **pathSum = (int **)malloc(sizeof(int *) * gridSize);
+//     for (int i = 0; i < n; i++)
+//     {
+//         pathSum[i] = (int *)malloc(sizeof(int) * m);
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             if(i<1||j<1)
+//             {
+//                 pathSum[i][j] = grid[i][j];
+//                 continue;
+//             }
+//             if (i == 0)
+//             {
+
+//                 pathSum[i][j] = grid[i][j] + pathSum[i][j - 1];
+//                 continue;
+//             }
+//             if (j == 0)
+//             {
+//                 pathSum[i][j] = grid[i][j] + pathSum[i - 1][j];
+//                 continue;
+//             }
+//             pathSum[i][j] = grid[i][j] + (pathSum[i][j - 1] > pathSum[i - 1][j] ? pathSum[i - 1][j] : pathSum[i][j - 1]);
+//         }
+//     }
+
+//     return pathSum[n - 1][m - 1];
+// }
+
 int minPathSum(int **grid, int gridSize, int *gridColSize)
 {
     int n = gridSize;
@@ -14,23 +51,23 @@ int minPathSum(int **grid, int gridSize, int *gridColSize)
     {
         for (int j = 0; j < m; j++)
         {
-            if(i<1||j<1)
+            if (i < 1 && j < 1)
             {
-                pathSum[i][j] = grid[i][j];
+                pathSum[i][j] = *((int *)grid + i * m + j);
                 continue;
             }
             if (i == 0)
             {
 
-                pathSum[i][j] = grid[i][j] + pathSum[i][j - 1];
+                pathSum[i][j] = *((int *)grid + i * m + j) + pathSum[i][j - 1];
                 continue;
             }
             if (j == 0)
             {
-                pathSum[i][j] = grid[i][j] + pathSum[i - 1][j];
+                pathSum[i][j] = *((int *)grid + i * m + j) + pathSum[i - 1][j];
                 continue;
             }
-            pathSum[i][j] = grid[i][j] + pathSum[i][j - 1] > pathSum[i - 1][j] ? pathSum[i - 1][j] : pathSum[i][j - 1];
+            pathSum[i][j] = *((int *)grid + i * m + j) + (pathSum[i][j - 1] > pathSum[i - 1][j] ? pathSum[i - 1][j] : pathSum[i][j - 1]);
         }
     }
 
