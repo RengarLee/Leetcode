17124d82e86bfd3e78b32b2285c7e178a8c16e6f
Rengar Lee <RengarLee@163.com>
2020-12-24 17:38:52 +0800

    Imporve

--- 931/MinimumFallingPathSum.c
+++ 931/MinimumFallingPathSum.c
@@ -20,50 +20,96 @@ int MinTow(int a, int b)
     return a > b ? b : a;
 }
 
+// int minFallingPathSum(int **A, int ASize, int *AColSize)
+// {
+//     int n = ASize;
+//     int m = *AColSize;
+//     int **pathSum = (int **)malloc(sizeof(int *) * n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         pathSum[i] = (int *)malloc(sizeof(int) * m);
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             if (i == 0)
+//             {
+//                 pathSum[i][j] = A[i][j];
+//                 continue;
+//             }
+//             if (j == 0)
+//             {
+//                 if (j + 1 < m)
+//                 {
+//                     pathSum[i][j] = MinTow(pathSum[i - 1][j], pathSum[i - 1][j + 1]) + A[i][j];
+//                 }
+//                 else
+//                 {
+//                     pathSum[i][j] = pathSum[i - 1][j] + A[i][j];
+//                 }
+//                 continue;
+//             }
+//             if (j + 1 == m)
+//             {
+//                 pathSum[i][j] = MinTow(pathSum[i - 1][j - 1], pathSum[i - 1][j]) + A[i][j];
+//                 continue;
+//             }
+//             pathSum[i][j] = MinThree(pathSum[i - 1][j - 1], pathSum[i - 1][j], pathSum[i - 1][j + 1]) + A[i][j];
+//         }
+//     }
+//     int min = pathSum[n-1][0];
+//     for (int i = 0; i < m; i++)
+//     {
+//         if(min > pathSum[n-1][i])
+//         {
+//             min = pathSum[n-1][i];
+//         }
+//     }
+//     return min;
+// }
+
+
+// int minFallingPathSum(int **A, int ASize, int *AColSize)
+
 int minFallingPathSum(int **A, int ASize, int *AColSize)
 {
     int n = ASize;
     int m = *AColSize;
-    int **pathSum = (int **)malloc(sizeof(int *) * n);
-    for (int i = 0; i < n; i++)
-    {
-        pathSum[i] = (int *)malloc(sizeof(int) * m);
-    }
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             if (i == 0)
             {
-                pathSum[i][j] = *((int *)A + i * m + j);
                 continue;
             }
             if (j == 0)
             {
                 if (j + 1 < m)
                 {
-                    pathSum[i][j] = MinTow(pathSum[i - 1][j], pathSum[i - 1][j + 1]) + *((int *)A + i * m + j);
+                    A[i][j] = MinTow(A[i - 1][j], A[i - 1][j + 1]) + A[i][j];
                 }
                 else
                 {
-                    pathSum[i][j] = pathSum[i - 1][j] + *((int *)A + i * m + j);
+                    A[i][j] = A[i - 1][j] + A[i][j];
                 }
                 continue;
             }
             if (j + 1 == m)
             {
-                pathSum[i][j] = MinTow(pathSum[i - 1][j - 1], pathSum[i - 1][j]) + *((int *)A + i * m + j);
+                A[i][j] = MinTow(A[i - 1][j - 1], A[i - 1][j]) + A[i][j];
                 continue;
             }
-            pathSum[i][j] = MinThree(pathSum[i - 1][j - 1], pathSum[i - 1][j], pathSum[i - 1][j + 1]) + *((int *)A + i * m + j);
+            A[i][j] = MinThree(A[i - 1][j - 1], A[i - 1][j], A[i - 1][j + 1]) + A[i][j];
         }
     }
-    int min = pathSum[n-1][0];
+    int min = A[n-1][0];
     for (int i = 0; i < m; i++)
     {
-        if(min > pathSum[n-1][i])
+        if(min > A[n-1][i])
         {
-            min = pathSum[n-1][i];
+            min = A[n-1][i];
         }
     }
     return min;
